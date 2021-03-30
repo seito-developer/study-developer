c0a1dca220e7ceb8f45ee83265c1e26e1ff0d479
seito-developer <seito.horiguchi@gmail.com>
2021-03-30 18:16:14 +0900

    fixed storybook error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,20 @@
+const path = require("path")
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ['../my-app/components/**/*.stories.tsx'],  // どのstoryファイルを読み込むのか
+  webpackFinal: async (config) => {
+    config.module.rules = [
+      // デフォルトのrulesに入っているCSS用の設定が悪さをするのでお帰りいただく
+      ...config.module.rules.filter(rule => rule.test.source !== (/\.css$/).source),
+      // css-loader を設定しなおす
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+    config.resolve.alias = {
+      "@": path.resolve(__dirname, "..")      // こっちは私の趣味です
+    }
+    return config
+  }
 }
